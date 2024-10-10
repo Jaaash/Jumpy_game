4a6ba9272ab389f9b987dc6cc1ada89c4d33faff
Jaaash <38756373+Jaaash@users.noreply.github.com>
2024-10-10 14:42:36 +0100

    started again from scratch
    haha hehe

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+    public float speed = 5.0f;
+    public float jumpForce = 8.0f;
+    public float airControlForce = 10.0f;      // public variables can be adjusted from within Unity editor, under the 'Components' panel.
+    public float airControlMax = 1.5f;
+    Vector2 boxExtents;        // Variable to contain the vector info for the outer bounds of the BoxCollider
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        boxExtents = GetComponent<BoxCollider2D>().bounds.extents; // sets the boxExtents variable
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        // check if we are on the ground
+        Vector2 bottom =
+        new Vector2(transform.position.x, transform.position.y - boxExtents.y);
+
+        Vector2 hitBoxSize = new Vector2(boxExtents.x * 2.0f, 0.05f);
+
+        RaycastHit2D result = Physics2D.BoxCast(bottom, hitBoxSize, 0.0f,
+        new Vector3(0.0f, -1.0f), 0.0f, 1 << LayerMask.NameToLayer("Ground"));
+
+        bool grounded = result.collider != null && result.normal.y > 0.9f;
+        if (grounded)
+        {
+            if (Input.GetAxis("Jump") > 0.0f)
+                rigidBody.AddForce(new Vector2(0.0f, jumpForce), ForceMode2D.Impulse);
+            else
+                rigidBody.velocity = new Vector2(speed * h, rigidBody.velocity.y);
+        }
+        else
+        {
+            // allow a small amount of movement in the air
+            float vx = rigidBody.velocity.x;
+            if (h * vx < airControlMax)
+                rigidBody.AddForce(new Vector2(h * airControlForce, 0));
+        }
+    }
+} 
+
